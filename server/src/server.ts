79b0c5926a1f039d530a5d1cbddab9d63d1db7a6
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 17:04:00 -0300

    fix: Configure express

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
 import express from 'express'
+
+const app = express()
+
+app.get('/users', (req, res) => {
+    return res.send('teste')
+})
+
+app.listen(3333, () => {
+    console.log('Server running on port 3333');
+})
