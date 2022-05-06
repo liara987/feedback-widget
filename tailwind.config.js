74f63bfb60682c983224afa30eac6b4f213a0b1a
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 01:24:58 -0300

    fix: Addin new tailwind library and change default value of borderradius md

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          500: '#8257e6'
+          300: '#996DFF',
+          500: '#8257E6'
         }
+      },
+      borderRadius:{
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
 }
