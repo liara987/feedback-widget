079f9239851762aebc4ce75641598428066ed24c
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 00:52:16 -0300

    fix: Call widgetform component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,14 +1,14 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForn'
 
 export function Widget() {
 	return (
-		<Popover className='absolute bottom-6 right-6'>
-			<Popover.Panel>teadfgasdgs</Popover.Panel>
+		<Popover className='absolute bottom-6 right-6 md:bottom-8 md:right-8 flex flex-col items-end'>
+			<Popover.Panel><WidgetForm/></Popover.Panel>
 			<Popover.Button className='bg-brand-500 rounded-full px-3 h-12 flex items-center group'>
 				<ChatTeardropDots className='w-6 h-6 text-white' />
 				<span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
-
 					<span className='text-white'>Feedback</span>
 				</span>
 			</Popover.Button>
