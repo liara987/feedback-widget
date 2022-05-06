407e2a84405fd5604296dfb74e8c0bb8842f550c
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 13:24:01 -0300

    fix: Adding funtionality to feedback suceess screen

--- src/components/WidgetForn/index.tsx
+++ src/components/WidgetForn/index.tsx
@@ -37,13 +37,14 @@ export function WidgetForm() {
     const [feedbackSent, setFeedbackSent] = useState(false)
 
     function handdleFeedbackRestart() {
+        setFeedbackSent(false )
         setFeedbackType(null)
     }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {feedbackSent ? (
-                <FeedbackSuccessStep />
+                <FeedbackSuccessStep onFeedbackRestartRequested={handdleFeedbackRestart} />
             ) : (
                 <>
                     {!feedbackType ? (
