ccaad18f5e8509732e3cfd71a52c0324dd24bd66
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 00:53:31 -0300

    feature: Create WidgetForm and add  funcionality to select feedback type

---
+++ src/components/WidgetForn/index.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import bugIcon from '../../assets/bug.png'
+import ideaIcon from '../../assets/idea.png'
+import thoughtIcon from '../../assets/thought.png'
+import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
+
+export const feedbackTypes = {
+    bug: {
+        title: "Problema",
+        image: {
+            src: bugIcon,
+            alt: 'Icone de um inseto'
+        }
+    },
+    idea: {
+        title: "Ideia",
+        image: {
+            src: ideaIcon,
+            alt: 'Icone de uma lampada'
+        }
+    },
+    others: {
+        title: "Outros",
+        image: {
+            src: thoughtIcon,
+            alt: 'Icone de uma nuvem de pensamento'
+        }
+    }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handdleFeedbackRestart (){
+        setFeedbackType(null)
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">            
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}></FeedbackTypeStep>
+            ) : (
+                <FeedbackContentStep feedbackType={feedbackType} onFeedbackRestart={handdleFeedbackRestart}/>
+            )}
+            <footer className="text-xs text-neutral-400">
+                Feito com ♥ pela <a className="underline underline-offset-1" href="https://github.com/liara987">Liara Programadora</a>
+            </footer>
+        </div>
+    )
+}
