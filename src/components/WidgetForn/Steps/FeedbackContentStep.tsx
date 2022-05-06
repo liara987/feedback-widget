2c763925c9c28b974d5cb8c23b085bca4660fbcf
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-06 01:23:59 -0300

    fix: Adding text area to FeedbackContentStep

--- src/components/WidgetForn/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForn/Steps/FeedbackContentStep.tsx
@@ -26,9 +26,20 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestart }: Feedbac
 
                 <ButtonClose />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-
-            </div>
+            <form className="my-4 w-full">
+                <textarea
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Descreva aqui o problema"
+                />
+                <footer className="flex gap-2 mt-2">
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        Enviar Feedback
+                    </button>
+                </footer>
+            </form>
         </>
     )
 }
