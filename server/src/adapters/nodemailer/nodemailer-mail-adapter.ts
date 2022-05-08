a52e4a2b34561abfaafebfe348321420c6295982
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-08 13:11:27 -0300

    feature: Create nodemailer adapter

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "7e535cd142112e",
+        pass: "10592b99008cd5"
+    }
+});
+export class NodemailerMailAdapter implements MailAdapter {
+
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe feedback <liara@email.com>',
+            to: 'Liara duarte <987.liara@gmail.com>',
+            subject,
+            html: body
+        })
+    }
+}
