07f942ee788497a0de29332a0da706e782dca8bf
Liara Pereira Duarte <lipereira@uolinc.com>
2022-05-09 00:01:18 -0300

    chore: Add port env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('Server running on port 3333');
 })
